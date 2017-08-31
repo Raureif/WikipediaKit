f895e92cef00ef1d6bf4301957f53ed34898d96b
Frank Rausch <git@frankrausch.com>
2017-08-31 11:54:00 +0200

    Tweak maximum length for truncating Most Read URLs.

--- Sources/Wikipedia+MostRead.swift
+++ Sources/Wikipedia+MostRead.swift
@@ -175,7 +175,7 @@ extension Wikipedia {
             // and cause the NSURLSession to fail.
             // This happens often for languages where Cyrillic is URL-encoded.
             // See https://stackoverflow.com/a/417184
-            titlesString = String(titlesString.characters.prefix(1000))
+            titlesString = String(titlesString.characters.prefix(950))
         }
         
         let parameters: [String:String] = [
