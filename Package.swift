7ba02825c2a4790353f8a933a7ba1bcab79a8981
Frank Rausch <git@frankrausch.com>
2019-11-25 14:51:36 +0100

    Fix syntax error in Package.swift.

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         .testTarget(
             name: "WikipediaKitTests",
             dependencies: ["WikipediaKit"],
-            path: "WikipediaKitTests"),
+            path: "WikipediaKitTests"
             ),
     ]
 )
